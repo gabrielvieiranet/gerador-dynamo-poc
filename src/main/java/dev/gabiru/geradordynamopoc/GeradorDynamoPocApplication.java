5e4b0ccc7ebd9f7dd9ca29fc17942408146cbbd1
gabiru <forrest10ster@gmail.com>
2023-11-10 12:29:17 -0300

    initial commit

---
+++ src/main/java/dev/gabiru/geradordynamopoc/GeradorDynamoPocApplication.java
@@ -0,0 +1,13 @@
+package dev.gabiru.geradordynamopoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDynamoPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDynamoPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gabiru/geradordynamopoc/GeradorDynamoPocApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabiru.geradordynamopoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorDynamoPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
